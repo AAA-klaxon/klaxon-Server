1e8ea2322b9e5ab86362ba440a8b8b0a7d103aa2
Jaen-923 <dhrwodms09@naver.com>
2024-08-17 23:47:38 +0900

    Feat : 웹 후크 테스트

--- config/db.js
+++ config/db.js
@@ -4,9 +4,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const pool = await mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  port: process.env.DB_PORT,
+  host: process.env.DB_HOST, // local
+  user: process.env.DB_USER, //root
+  port: process.env.DB_PORT, 
   database: process.env.DB_TABLE,
   password: process.env.DB_PASSWORD,
   waitForConnections: true,
