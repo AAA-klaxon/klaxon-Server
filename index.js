64dd7f839ce831767817f27f581fa401015298c1
yeuuni <142524724+yeuuni@users.noreply.github.com>
2024-09-11 19:49:14 +0900

    Update index.js
    수정코드

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// srcs/index.js
 import express from 'express';
 import bodyParser from 'body-parser';
 import authRoutes from './srcs/auth/auth.route.js';
@@ -16,8 +15,11 @@ app.use('/users', userRoutes);
 app.use('/community', communityRoutes);
 app.use('/errors', errorRoutes);
 
+// 기본 경로에 대한 처리
+app.get('/', (req, res) => {
+  res.send('Hello, World! 서버가 정상적으로 동작하고 있습니다.');
+});
+
 app.listen(port, '0.0.0.0', () => {
   console.log(`서버가 http://0.0.0.0:${port}에서 실행 중입니다.`);
 });
-
-
